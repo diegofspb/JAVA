27a9bef4da9b5a0e68b7ad3604c99f34b252dabd
Diego <diegofariasilva123@Gmail.com>
2023-10-02 18:03:25 -0300

    finalizando um para muitos

--- exercicios-jpa/src/Modelo/umParaMuitos/ItemPedido.java
+++ exercicios-jpa/src/Modelo/umParaMuitos/ItemPedido.java
@@ -8,10 +8,17 @@ public class ItemPedido {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne // 'um ou vários' 'ItemPedido' associados a apenas um 'pedido'.
+    @ManyToOne // 'um ou vários' 'ItemPedido' associados a apenas um 'pedido', ou seja, várias instâncias de 'ItemPedido' podem se referir à mesma instância de 'pedido'
     private Pedido pedido; // será criado na tabela 'itempedido' a coluna 'pedido_id'
-    @ManyToOne // 'um ou vários' 'ItemPedido' associados a apenas um 'produto'.
+    @ManyToOne(fetch = FetchType.EAGER) // @ManyToOne significa 'um ou vários' 'ItemPedido' associados ao mesmo 'produto'
     private Produto produto; // será criado na tabela 'itempedido' a coluna 'produto_id'
+    // tanto 'produto' quanto 'pedido' existem apenas 1 em relação ao item do pedido, então quando vc faz uma consulta em ItemPedido, por padrão ele
+    // executa uma consulta tipo EAGER (consulta tudo), já se fosse produto ou pedido fossem vários, então deveria ser uma consulta 'lazy'(tardia)
+    // vi o exemplo de quando vc executa uma consulta tipo EAGER ele trás todos os dados da tabela pesquisada + os dados das tabelas que se relacionam com
+    // aquele registro, porém é possível que esta consulta traga uma quantidade grande de dados e trave ou deixe o sistema lento, por isso motivo
+    // é indicado usar a consulta do lado 'one' ser EAGLE, porém se o lado da consulta for Many então o ideal é 'lazy'. Este lazy não carregará
+    // tudo, apenas o que for o principal e para carregar os dados das tabelas que estão se relacionando então terá que fazer uma subconsulta para pegar estes dados
+    // um exemplo aqui foi a lista de itens que é uma relaçao OneToMany
     @Column(nullable = false)
     private int quantidade;
     @Column(nullable = false)
